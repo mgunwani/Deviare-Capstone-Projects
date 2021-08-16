b900c3013db4f862d9828b8700eea22da1a67d39
bgunwani <bhawna.gunwani@gmail.com>
2021-08-16 17:20:03 +0530

    Capstone Day One Commit

---
+++ angular-dayOne/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    HomeComponent,
+    MainComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [MainComponent]
+})
+export class AppModule { }
